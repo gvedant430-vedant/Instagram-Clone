32d3134a559f720ec5a44acc48c3dc90f58364a1
Vedant Gupta <gvedant430@gmail.com>
2026-07-31 17:39:29 +0530

    Updated

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,6 +1,30 @@
-function Explore(){
-  return(
-    <h1>Explore</h1>
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import "../css/explore.css";
+function Explore() {
+  const images = [
+    "https://picsum.photos/300?1",
+    "https://picsum.photos/300?2",
+    "https://picsum.photos/300?3",
+    "https://picsum.photos/300?4",
+    "https://picsum.photos/300?5",
+    "https://picsum.photos/300?6",
+  ];
+
+  return (
+    <>
+      <Navbar />
+
+      <div className="home-layout">
+        <Sidebar />
+
+        <div className="explore-grid">
+          {images.map((img, index) => (
+            <img key={index} src={img} alt="explore" />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,34 @@
-function Profile(){
-    return(
-        <h1>Profile Page</h1>
-    );
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import "../css/profile.css";
+function Profile() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="home-layout">
+        <Sidebar />
+
+        <div className="profile-content">
+          <img
+            src="https://i.pravatar.cc/200?img=10"
+            alt="profile"
+            className="profile-avatar"
+          />
+
+          <h2>vedant</h2>
+
+          <div className="profile-page-stats">
+            <span><strong>5</strong> Posts</span>
+            <span><strong>100</strong> Followers</span>
+            <span><strong>90</strong> Following</span>
+          </div>
+
+          <p>Frontend Developer 🚀</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Profile;
