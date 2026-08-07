9ddddfd77f04368326f40e722cbee5cdb971da6b
Vedant Gupta <gvedant430@gmail.com>
2026-08-07 10:47:16 +0530

    Updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,39 @@
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
-import Stories from "../components/Stories";
 import Post from "../components/Post";
 import ProfileCard from "../components/ProfileCard";
 import SuggestedUsers from "../components/SuggestedUsers";
+
 import "../css/home.css";
 
-function Home(){
-    return(
-        <div className="home-container">
-            {/* <h1>Home Page</h1> */}
-            <Navbar />
+function Home() {
+  return (
+    <div className="home-container">
 
-            <div className="home-layout">
-                <Sidebar />
-                <main className="feed">
-                    <Stories />
-                    <Post/>
-                    
-                </main>
-            
+      <Navbar />
 
-                <div className="profile-section">
-                  <ProfileCard />
-                      <SuggestedUsers />
-                       </div>
-            </div>
+      <div className="home-layout">
 
-        </div>
-        
-    );
+        <Sidebar />
+
+        {/* Feed */}
+        <main className="feed">
+          <Post />
+        </main>
+
+        {/* RIGHT SIDE */}
+        <aside className="profile-section">
+
+          <ProfileCard />
+
+          <SuggestedUsers />
+
+        </aside>
+
+      </div>
+
+    </div>
+  );
 }
+
 export default Home;
